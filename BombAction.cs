36f0b151c940d0e64ebc9c4bbbc9389a9cfa2422
byObin <byObin26@gmail.com>
2022-01-18 23:50:39 +0900

    FPS_13

--- BombAction.cs
+++ BombAction.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class BombAction : MonoBehaviour
 {
+    public GameObject bombEffect;   //폭발 이펙트 프리팹 변수
+
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(gameObject);
+        GameObject eff = Instantiate(bombEffect);   // 이펙트 프리팹 생성
+
+        eff.transform.position = transform.position; // 이펙트 프리팹 위치는 수류탄 위치와 동일
+
+        Destroy(gameObject);    //자기 자신 제거
     }
 
     void Start()
